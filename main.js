ea9d7a39e2b01045e667c658ce894d025792f171
John Ammon <ammonj78@gmail.com>
2022-10-28 14:48:35 -0600

    Saves araays for food, adds to the css, starts js

---
+++ main.js
@@ -0,0 +1,3 @@
+///When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
+
+var radeoButtons document.querySelectore('.radeo-buttons')
